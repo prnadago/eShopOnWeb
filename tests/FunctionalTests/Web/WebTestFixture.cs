d754e67abee23bb1b9bd68726fcdfdbd6dd90eb8
pratiknadagouda <pratik.nadagouda@gmail.com>
2020-07-31 15:44:29 -0700

    This is a sample commit which is really long. This line of text will go on for quite long and hopefully overflow over the edge of the maximum width for single line texts in the history view in Visual Studio's new Git repository window. There are other columns in this view such as ID, Author, Date, but they shouldn't take up that much space. It's mostly the commit message.
    I've added a second line of text here with one empty line in between.
    Here is another line of text that is right below the previous line of text.

--- tests/FunctionalTests/Web/WebTestFixture.cs
+++ tests/FunctionalTests/Web/WebTestFixture.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using System;
 
-namespace Microsoft.eShopWeb.FunctionalTests.Web
+namespace Microsoft.eShopWeb.FunctionalTests.Web 
 {
     public class WebTestFixture : WebApplicationFactory<Startup>
     {
